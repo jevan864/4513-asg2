ca99c13f3dd18d8aa8b23f2df7dbae88f72b1fc8
jevan864 <jevan864@mtroyal.ca>
2024-04-08 22:52:04 -0600

    made all displayed lists reversible

--- src/components/RaceList.jsx
+++ src/components/RaceList.jsx
@@ -1,9 +1,18 @@
+import { useEffect, useState } from 'react';
 import RaceListItem from './RaceListItem.jsx'
 
 
 
 const RaceList = (props) =>{
-	
+	const [list, setList] = useState([props.data])
+
+	useEffect( () => {
+		setList(props.data);
+	}, [props.data] );
+	const reverseList=()=>{
+		const newList = [...list];
+		setList(newList.reverse());
+	}
 	return(
 		<div className='w-full justify-items-stretch content-start grid grid-cols-1 border-2 border-blue-800'>
 			<div className='w-full h-12'>
@@ -13,13 +22,17 @@ const RaceList = (props) =>{
 			<ul className='w-full'>
 				<li className="border-2 border-sky-300 grid grid-cols-2">
 					<div className="flex justify-items-start justify-self-start gap-4">
-						<div className="border-x-2 border-sky-300 font-bold">Rnd</div>
-						<div className='font-bold'>Circuit</div>
+						<div className="border-x-2 border-sky-300">
+						<button className="text-center align-middle transition-all rounded-lg select-none hover:bg-slate-400/40 active:bg-slate-400/20" type="button" onClick={reverseList}>
+							{"Rnd."}
+						</button>
+						</div>
+						<div>Circuit</div>
 					</div>
 					<div className="grid justify-self-end align-middle grid-cols-2">
 					</div>
 				</li>
-				{ props.data.map( (r,indx) => <RaceListItem race={r} key={indx} update={props.update}/>) }
+				{ list.map( (r,indx) => <RaceListItem race={r} key={indx} update={props.update}/>) }
 			</ul>
 			</div>
 		</div>
--- src/components/ResultsList.jsx
+++ src/components/ResultsList.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react';
 import ResultsListItem from './ResultsListItem.jsx'
 import ResultsHeader from './ResultsHeader.jsx'
 
 
 
 const ResultsList = (props) =>{
+	const [list, setList] = useState([{positionText:"", laps:"", points:"", constructors:{name:""}, drivers:{forename:"", surname:""}}]);
+
+	useEffect( () => {
+		setList(props.data);
+	}, [props.data] );
+	const reverseList=()=>{
+		const newList = [...list];
+		setList(newList.reverse());
+	}
+	console.log(list);
 	return(
 		<div className='w-full flex content-start text-sm grid grid-cols-1'>
 			<div className='w-5/6 grid grid-cols-3 items-end text-xl justify-self-center gap-2 mb-2'>
@@ -32,7 +43,11 @@ const ResultsList = (props) =>{
 			<ul className='w-full'>
 				<li className="border-2 border-sky-300 flex items-center">
 					<div className="flex justify-items-start grid grid-cols-5 w-8">
-						<div className="border-x-2 border-sky-300 justify-items-start w-8">{"Pos."}</div>
+						<div className="border-x-2 border-sky-300 justify-items-start w-8">
+						<button className="text-center align-middle transition-all rounded-lg select-none hover:bg-slate-400/40 active:bg-slate-400/20" type="button" onClick={reverseList}>
+							{"Pos."}
+						</button>	
+						</div>
 					</div>
 					<div className="flex grid grid-cols-5 w-full">
 						<div className="justify-self-start justify-items-start col-span-2 ml-2">{"Driver Name"}</div>
@@ -42,7 +57,7 @@ const ResultsList = (props) =>{
 						<div>{"Pts."}</div>
 					</div>
 				</li>
-				{ props.data.map( (d,indx) => <ResultsListItem data={d} key={indx} modal={props.modal}/>) }
+				{ list.map( (d,indx) => <ResultsListItem data={d} key={indx} modal={props.modal}/>) }
 			</ul>
 		</div>
 	)
